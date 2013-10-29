11ed105adbae29e60e4ec417cd725f2e6a669a38
hiraken <htb@mvf.biglobe.ne.jp>
2013-10-29 21:35:39 +0900

    Initial Commit

---
+++ openCVTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  openCVTest
+//
+//  Created by HIRAMATSU KENJIRO on 2013/10/29.
+//  Copyright (c) 2013年 HIRAMATSU KENJIRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
